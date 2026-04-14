339c6f5b4940190e5d969268f664a7e2bb6e2bb1
admin <mogshipenterprise@outlook.com>
2026-04-14 00:25:22 -0400

    New Style
    Updated style and removed unused assets for the site.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,56 @@
 $(document).ready(function(){
+	var siteHeader = document.querySelector("header");
+	if (siteHeader) {
+		var toggleHeaderState = function() {
+			if (window.scrollY > 40) {
+				siteHeader.classList.add("is-scrolled");
+			} else {
+				siteHeader.classList.remove("is-scrolled");
+			}
+		};
 
+		toggleHeaderState();
+		window.addEventListener("scroll", toggleHeaderState);
+	}
 
-	$("#portfolio-contant-active").mixItUp();
+	if ($("#portfolio-contant-active").length) {
+		$("#portfolio-contant-active").mixItUp();
+	}
 
+	if ($(".counter").length) {
+		$(".counter").counterUp({
+	        delay: 10,
+	        time: 1000
+	    });
+	}
 
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
+	var scrollTopButton = document.getElementById("scrollTopButton");
+	if (scrollTopButton) {
+		var toggleScrollButton = function() {
+			if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+				scrollTopButton.classList.add("is-visible");
+			} else {
+				scrollTopButton.classList.remove("is-visible");
+			}
+		};
 
+		toggleScrollButton();
+		window.addEventListener("scroll", toggleScrollButton);
+		scrollTopButton.addEventListener("click", function() {
+			window.scrollTo({ top: 0, behavior: "smooth" });
+		});
+	}
 
-
-
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
+	$('a[href^="#"]').on("click", function(event) {
+		var targetId = $(this).attr("href");
+		if (targetId.length > 1 && $(targetId).length) {
+			event.preventDefault();
+			$("html, body").animate({
+				scrollTop: $(targetId).offset().top - 80
+			}, 500);
 		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
+	});
 
 });
-
-
 
 
